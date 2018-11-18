06366edd86da5dec6fcd5c00b80961724d325f3b
v.ivanov <ivanovvlad.261996@mail.ru>
2018-11-19 00:01:12 +0300

    Добавление инпута для поиска

--- src/main/java/gwt/client/Application.java
+++ src/main/java/gwt/client/Application.java
@@ -2,6 +2,10 @@ package gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 import gwt.client.service.ApplicationServiceAsync;
 import gwt.client.text.ApplicationConstants;
@@ -16,10 +20,31 @@ public class Application implements EntryPoint {
 
     private static ApplicationServiceAsync service = INSTANCE.getService();
     private static ApplicationConstants dictionary = INSTANCE.getConstants();
+    private static TextBox tb = new TextBox();
+
 
     public void onModuleLoad() {
         initHeaderAndTitle();
         initMainSlot();
+        searchText();
+    }
+
+    private void searchText(){
+        VerticalPanel mainPanel = new VerticalPanel();
+        mainPanel.add(tb);
+        tb.setName("search");
+        tb.addKeyDownHandler(keyDownEvent -> {
+            if (keyDownEvent.getNativeKeyCode() == KeyCodes.KEY_ENTER){
+                Window.Location.replace("https://www.google.ru/search?" +
+                        "newwindow=1&" +
+                        "source=hp&ei=LR7wW7juOKWyrgT_6JnABw&" +
+                        "q=" + tb.getValue() +
+                        "&btnK=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA+%D0%B2+Google&" +
+                        "pq=" + tb.getValue() +
+                        "&gs_l=");
+            }
+        });
+        RootPanel.get("searchInput").add(mainPanel);
     }
 
 
